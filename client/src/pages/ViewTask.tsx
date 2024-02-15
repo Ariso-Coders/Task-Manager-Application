d636fd473199b3311d0a866a4cf0875ce8f6e46e
TharakaJayz <tharakaprabhath300@gmail.com>
2024-02-15 11:44:13 +0530

    styling finisehd

--- client/src/pages/ViewTask.tsx
+++ client/src/pages/ViewTask.tsx
@@ -231,21 +231,22 @@ const ViewTask = () => {
   };
 
   return (
-    <div className="w-full">
-      <div className="w-full flex flex-col md:flex-row gap-4 md:gap-8 mt-view_task_4 ml-view_task_6 md:ml-4xl items-center">
-        <input
-          placeholder="Search For Task"
-          className="w-2/3 md:w-3/4 border border-gray p-view_task_1 rounded-md mb-4 md:mb-0"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <div className="relative">
-          <FaFilter
-            className="text-4xl hover:cursor-pointer"
-            onClick={toggleFilterMenu}
+    <div className="w-full min-h-screen flex flex-col justify-start px-3 gap-20 pb-10">
+      <div className="w-full">
+        <div className="w-full flex flex-col md:flex-row gap-4 md:gap-8 mt-view_task_4  md:ml-4xl items-center ">
+          <input
+            placeholder="Search For Task"
+            className="w-2/3 md:w-3/4 border border-gray p-view_task_1 rounded-md mb-4 md:mb-0"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          {isFilterMenuOpen && (
-            <div className="w-full absolute top-full right-0 mt-2 bg-white p-2 rounded-md opacity-80">
+          <div className="relative">
+            <FaFilter
+              className="text-4xl hover:cursor-pointer"
+              onClick={toggleFilterMenu}
+            />
+            {/* {isFilterMenuOpen && (
+            <div className="absolute top-0 right-0  bg-red p-5 rounded-md opacity-80 ">
               <div className="flex flex-col">
                 <div className="flex items-center mb-4">
                   <p className="mr-2">Filter by:</p>
@@ -276,64 +277,66 @@ const ViewTask = () => {
                     <DateRangePicker
                       ranges={[selectionRange]}
                       onChange={handleDateRange}
+                      className="w-full"
                     />
                   </div>
                 </div>
               </div>
             </div>
-          )}
+          )} */}
+          </div>
+          <MdLogout
+            className="text-5xl hover:cursor-pointer"
+            onClick={logoutHandler}
+          />
         </div>
-        <MdLogout
-          className="text-5xl hover:cursor-pointer"
-          onClick={logoutHandler}
-        />
+        <div className=" w-full flex flex-col md:flex-row mt-4  gap-4 md:gap-8 items-center ">
+          <h1 className="font-bold text-3xl mb-4 ml-60 md:mb-0 	text-transform:capitalize">
+            {`You have got ${taskCount} tasks `}
+            {isOverDue.logic && (
+              <span className="text-over_due hover:underline hover:cursor-pointer" onClick={() => { setFilteredTasks(isOverDue.values) }}>
+                And You Have {isOverDue.values.length} Overdue{" "}
+                {isOverDue.values.length > 1 ? "Tasks" : "Task"}
+              </span>
+            )}{" "}
+          </h1>
+          <button
+            className="text-xl bg-view_task_main_color  md:p-1 rounded-md text-view_task_white font-bold flex items-center justify-center hover:bg-opacity-75 "
+            onClick={() => {
+              // navigation('/taskpopup');
+              setTaskOverLayLogic(true);
+            }}
+          >
+            <IoAddSharp className="size-8" />
+            <span className="">Add</span>
+          </button>
+        </div>
       </div>
-      <div className="flex flex-col md:flex-row mt-4 ml-6 md:ml-12 gap-4 md:gap-8 items-center">
-        <h1 className="font-bold text-3xl mb-4 ml-60 md:mb-0">
-          {`You have got ${taskCount} tasks `}
-          {isOverDue.logic && (
-            <span className="text-red" onClick={() => {setFilteredTasks(isOverDue.values)}}>
-              And You Have {isOverDue.values.length} Overdue{" "}
-              {isOverDue.values.length > 1 ? "Tasks" : "Task"}
-            </span>
-          )}{" "}
-        </h1>
-        <button
-          className="text-xl bg-view_task_main_color p-4 md:p-1 rounded-md text-view_task_white font-bold flex items-center justify-center hover:bg-opacity-75"
-          onClick={() => {
-            // navigation('/taskpopup');
-            setTaskOverLayLogic(true);
-          }}
-        >
-          <IoAddSharp />
-          <span className="ml-2 md:ml-1">Add</span>
-        </button>
-      </div>
-      <div className="w-full md:w-auto mx-auto flex md:flex-row mt-2 md:ml-30">
-        <div className="w-full md:w-auto mx-auto flex md:flex-row mt-14 md:ml-30">
-          <div className="text-xl w-full md:mb-0">
+      <div className="w-full flex items-center justify-center  px-20   h-view_task_13 overflow-y-scroll  ">
+        <div className="w-full ">
+          <div className="text-xl w-full flex flex-col justify-center gap-4 ">
             {filteredTasks.map((task: Task) => (
-              <div key={task._id} className="flex flex-row mb-4">
-                <div className="w-full overflow-x-auto break-all">
+              <div key={task._id} className=" w-full flex items-center hover:bg-task_hover px-5  py-2 hover:cursor-pointer ">
+                <div className="  flex-1 	text-transform: capitalize  text-left overflow-hidden">  {/* 1*/}
                   {task.task_description}
                 </div>
-                <div className="w-full overflow-x-auto break-all">
+                <div className="  flex-1 text-left">  {/* 2*/}
                   {task.date.split("T")[0]}
                 </div>
 
-                <div className="w-view_task_6 ">
+                <div className="w-view_task_6  flex-1 flex justify-start ">   {/* 3*/}
                   <button
                     onClick={() => {
                       deleteTaskHandler(task._id);
                     }}
-                    className="w-10 text-view_task_4 bg-view_task_main_color rounded-md text-view_task_white p-view_task_1 hover:bg-opacity-75"
+                    className="w-10 text-view_task_4 bg-view_task_main_color rounded-md text-view_task_white p-view_task_1 hover:bg-over_due"
                   >
                     <AiOutlineDelete />
                   </button>
                 </div>
-                <div>
-                  <label className="flex items-center">
-                    <span className="ml-8">Mark as Complete</span>
+                <div className=" flex-1 flex justify-start  ">  {/* 4*/}
+                  <label className="flex items-center ">
+                    <span className="">Mark as Complete</span>
                     <span>
                       <input
                         type="checkbox"
@@ -351,7 +354,7 @@ const ViewTask = () => {
                     </span>
                   </label>
                 </div>
-                <div>
+                <div className="  flex-1">  {/* 5*/}
                   {!editMode || editMode !== task._id ? (
                     <button
                       className="bg-view_task_main_color p-view_task_1 rounded-md text-view_task_white font-bold ml-view_task_10 hover:bg-opacity-75"
@@ -400,6 +403,46 @@ const ViewTask = () => {
               btn2: [true, "No"],
             }}
           />
+        )}
+
+        {isFilterMenuOpen && (
+          <div className="absolute top-20 right-10 bg-purple px-5 py-10 rounded-md">
+            <div className="flex flex-col">
+              <div className="flex items-center mb-4">
+                <p className="mr-2">Filter by:</p>
+                <div className="flex">
+                  <label className="inline-flex items-center mr-4">
+                    <input
+                      type="checkbox"
+                      className="form-checkbox h-5 w-5 text-blue-500"
+                      checked={showCompleted}
+                      onChange={() => setShowCompleted(!showCompleted)}
+                    />
+                    <span className="ml-2">Completed</span>
+                  </label>
+                  <label className="inline-flex items-center">
+                    <input
+                      type="checkbox"
+                      className="form-checkbox h-5 w-5 text-blue-500"
+                      checked={showNotCompleted}
+                      onChange={() => setShowNotCompleted(!showNotCompleted)}
+                    />
+                    <span className="ml-2">Not Completed</span>
+                  </label>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <p className="mr-2">Date:</p>
+                <div>
+                  <DateRangePicker
+                    ranges={[selectionRange]}
+                    onChange={handleDateRange}
+                    className="w-full"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         )}
       </div>
 
