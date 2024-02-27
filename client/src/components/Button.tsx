048a303bbc342adfad1c6953b120f94586825c45
Nethmi-24 <batawalann.20@uom.lk>
2024-02-27 09:53:23 +0530

    eslint first rules added

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,12 +1,16 @@
+
 interface ButtonProps {
   buttonLabel: string
 }
+
 export const Button = (props: ButtonProps) => {
   return (
     <div>
       <button className="bg-main_color py-2 px-6 rounded-md mb-4 text-white">
         Log in
       </button>
+     
     </div>
+   
   )
 }
