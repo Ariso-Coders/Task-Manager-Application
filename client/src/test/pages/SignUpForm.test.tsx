6f1484658dae6dd34075e8bbe7218ec443e1878a
Ashani T ranasingha <105184856+Aashani123@users.noreply.github.com>
2024-03-06 16:50:47 +0530

    SubmitHandler testing signup form

--- client/src/test/pages/SignUpForm.test.tsx
+++ client/src/test/pages/SignUpForm.test.tsx
@@ -3,9 +3,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import SignUp from "../../pages/SignUp";
 import { Button } from "../../components/Button";
-// import axios,{AxiosResponse} from "axios";
+import {FormData} from '../../pages/SignUp'
 
-// jest.mock('axios'); // Mock axios module
 
 test("SignUp component renders without crashing", () => {
   render(
@@ -101,33 +100,17 @@ describe("Error Messages ", () => {
   });
 });
 
+//submithandler tests
+test("Submit handler is rendered", async () => {
+  const submitHandler = jest.fn();
+  const props: FormData = {
+    handleSubmit: submitHandler,
+  };
 
-// describe("SignUp component", () => {
-//   test("submit handler is called when the form is submitted", async () => {
-//     render(<Router>
-//       <SignUp />
-//     </Router>);
-
-//     // Mock the axios response
-//     (axios.post as jest.Mock).mockResolvedValueOnce({ data: {} } as AxiosResponse);
-
-//     // Simulate user input
-//     fireEvent.change(screen.getByLabelText("Email"), { target: { value: 'test@example.com' } });
-//     fireEvent.change(screen.getByLabelText("Name"), { target: { value: 'Test User' } });
-//     fireEvent.change(screen.getByLabelText("DOB"), { target: { value: '2000-01-01' } });
-//     fireEvent.change(screen.getByLabelText("Password"), { target: { value: 'password' } });
-//     fireEvent.change(screen.getByLabelText("Re-enter password"), { target: { value: 'password' } });
-
-//     // Submit the form
-//     fireEvent.submit(screen.getByText("SignUp"));
-
-//     // Assert that axios.post has been called
-//     expect(axios.post).toHaveBeenCalledWith("http://localhost:8080/user/signup", {
-//       email: 'test@example.com',
-//       name: 'Test User',
-//       dob: '2000-01-01',
-//       password: 'password',
-//       confirmPassword: 'password'
-//     });
-//   });
-// });
+  render(
+    <Router>
+      <SignUp {...props} handleSubmit={submitHandler} />
+      <Button buttonLabel="SignUp" />
+    </Router>
+  )
+})
