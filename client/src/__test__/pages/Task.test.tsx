d4d8b377a7615a0cfaf951ea5df34ea41b6086d3
Ashani T ranasingha <105184856+Aashani123@users.noreply.github.com>
2024-03-11 11:56:41 +0530

    task page unit testing

--- client/src/__test__/pages/Task.test.tsx
+++ client/src/__test__/pages/Task.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render,screen,fireEvent } from "@testing-library/react";
 import React from "react";
 import Task from "../../pages/Task";
 import { Provider } from "react-redux";
@@ -10,4 +10,30 @@ test("Render The Task page without crashing", () => {
       <Task />
     </Provider>
   );
+});
+
+
+describe("Task component", () => {
+  test("renders Task component", () => {
+    render(<Task />);
+    expect(screen.getByText(/you have/i)).toBeInTheDocument();
+  });
+
+  test("displays error card when delete button is clicked", () => {
+    render(<Task />);
+    fireEvent.click(screen.getByText("Delete")); // Assuming the delete button text is "Delete"
+    expect(screen.getByText(/Do you want to Delete this task ?/i)).toBeInTheDocument();
+  });
+
+  test("updates search term state when user types in search input", () => {
+    render(<Task />);
+    fireEvent.change(screen.getByPlaceholderText("Search For Task"), { target: { value: "example" } });
+    expect(screen.getByPlaceholderText("Search For Task")).toHaveValue("example");
+  });
+
+  test("toggles filter menu when filter icon is clicked", () => {
+    render(<Task />);
+    fireEvent.click(screen.getByRole("button", { name: /filters/i }));
+    expect(screen.getByText(/Filter by:/i)).toBeInTheDocument();
+  });
 });
