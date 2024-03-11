86530fb0ce46c8437a7b796bddaf6db689500363
Ashani T ranasingha <105184856+Aashani123@users.noreply.github.com>
2024-03-11 11:11:23 +0530

    task unit testing

---
+++ client/src/__test__/pages/Task.test.tsx
@@ -0,0 +1,11 @@
+import {
+    render,
+    screen,
+    fireEvent,
+    getByPlaceholderText,
+    waitFor,
+  } from "@testing-library/react";
+  import React from "react";
+  import Task from '../../pages/Task'
+
+  
