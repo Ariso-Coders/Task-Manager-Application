6c2dc695058cc27f144bb6853ed0679ba370144c
Ashani T ranasingha <105184856+Aashani123@users.noreply.github.com>
2024-03-12 09:56:37 +0530

    unit testing

--- client/src/__test__/components/SideBar.test.tsx
+++ client/src/__test__/components/SideBar.test.tsx
@@ -1,31 +1,76 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; 
+import { MemoryRouter } from 'react-router-dom'; 
+import '@testing-library/jest-dom'; 
 import SideBar from '../../components/sidebar/SideBar';
-import { Router } from 'react-router-dom';
 
 const mockProps = {
-    onCallback: jest.fn(),
-    logoutLogic: false,
-  };
+  onCallback: jest.fn(),
+  logoutLogic: false,
+};
   
-  describe('SideBar Component', () => {
-    test('renders without crashing', () => {
-      render(<SideBar {...mockProps} />);
-      expect(screen.getByText(/ARISO Technologies/i)).toBeInTheDocument();
-    });
-    
-    test('renders navigation links', () => {
-        render(<SideBar {...mockProps} />);
-        expect(screen.getByText(/Task/i)).toBeInTheDocument();
-        expect(screen.getByText(/Users/i)).toBeInTheDocument();
-        expect(screen.getByText(/Settings/i)).toBeInTheDocument();
-      });
-    
-      test('calls onCallback when logout button is clicked', () => {
-        render(<SideBar {...mockProps} />);
-        fireEvent.click(screen.getByText(/Logout/i));
-        expect(mockProps.onCallback).toHaveBeenCalledTimes(1);
-        expect(mockProps.onCallback).toHaveBeenCalledWith(true);
-      });
-    });
+describe('SideBar Component', () => {
+  test('renders without crashing', () => {
+    render(
+      <MemoryRouter>
+        <SideBar {...mockProps} />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/ARISO Technologies/i)).toBeInTheDocument();
+  });
+  
+  test('renders navigation links', () => {
+    render(
+      <MemoryRouter>
+        <SideBar {...mockProps} />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/Task/i)).toBeInTheDocument();
+    expect(screen.getByText(/Users/i)).toBeInTheDocument();
+    expect(screen.getByText(/Settings/i)).toBeInTheDocument();
+  });
+  
+  test('calls onCallback when logout button is clicked', () => {
+    render(
+      <MemoryRouter>
+        <SideBar {...mockProps} />
+      </MemoryRouter>
+    );
+    fireEvent.click(screen.getByText(/Logout/i));
+    expect(mockProps.onCallback).toHaveBeenCalledTimes(1);
+    expect(mockProps.onCallback).toHaveBeenCalledWith(true);
+  });
+
+  test('navigates to "/task" page when Task link is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <SideBar {...mockProps} />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(screen.getByText(/Task/i));
+    expect(window.location.pathname).toBe('/task');
+  });
+
+  test('navigates to "/user" page when Task link is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <SideBar {...mockProps} />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(screen.getByText(/Users/i));
+    expect(window.location.pathname).toBe('/user');
+  });
+
+  test('navigates to "/task" page when Task link is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <SideBar {...mockProps} />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(screen.getByText(/Settings/i));
+    expect(window.location.pathname).toBe('/setting');
+  });
+});
